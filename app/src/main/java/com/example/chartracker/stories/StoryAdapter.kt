d78b8d5edcc855247a4458ac9212428188b8c59a
rhtakaha <rytakahashi97@gmail.com>
2023-12-02 18:26:06 -0800

    Got started with RecyclerView for Stories, but realized that you need to have the data object defined which we haven't figure out yet so coming back to this.

---
+++ app/src/main/java/com/example/chartracker/stories/StoryAdapter.kt
@@ -0,0 +1,7 @@
+package com.example.chartracker.stories
+
+import androidx.recyclerview.widget.RecyclerView
+
+//class StoryAdapter: RecyclerView.Adapter<TextItemHolder>() {
+//    var data = listOf<>()
+//}
