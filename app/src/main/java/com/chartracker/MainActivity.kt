821ad6870bbf8f6d0a356e25abdefd549b0fc1c4
rhtakaha <rytakahashi97@gmail.com>
2023-12-22 01:09:54 -0800

    FINALLY GOT THE ACTION BAR INTO AN UNDERSTANDBLE AND WORKABLE POSITION. FINISHING TOMORROW

--- app/src/main/java/com/chartracker/MainActivity.kt
+++ app/src/main/java/com/chartracker/MainActivity.kt
@@ -2,11 +2,28 @@ package com.chartracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.chartracker.R
+import android.util.Log
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.myNavHostFragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        NavigationUI.setupActionBarWithNavController(this, navController)
+
+        //REMOVES THE APP TITLE/ FRAGMENT TITLE FROM THE ACTION BAR
+        supportActionBar?.setDisplayShowTitleEnabled(false)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        Log.i("Main", "trying to back?")
+        val navController = this.findNavController(R.id.myNavHostFragment)
+        return navController.navigateUp()
     }
 }
--- app/src/main/java/com/chartracker/stories/StoriesFragment.kt
+++ app/src/main/java/com/chartracker/stories/StoriesFragment.kt
@@ -4,14 +4,21 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
+import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
+import com.chartracker.R
 import com.chartracker.databinding.FragmentStoriesBinding
 
 
-class StoriesFragment : Fragment() {
+class StoriesFragment : Fragment(), MenuProvider {
     private val tag = "StoriesFrag"
 
     private lateinit var viewModel: StoriesViewModel
@@ -57,7 +64,29 @@ class StoriesFragment : Fragment() {
             }
         }
 
+
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(this, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         return binding.root
+    }
+
+    override fun onPrepareMenu(menu: Menu) {
+        super.onPrepareMenu(menu)
+//        menu.clear()
+    }
+
+    override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+        // Add menu items here
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+    }
+
+    override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+        // Handle the menu selection
+        if (menuItem.itemId == android.R.id.home){
+            return false
+        }
+        return true
     }
 
 }
