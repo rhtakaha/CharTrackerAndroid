afeed31e9077c68977836a9334e256e86acb0eed
rhtakaha <rytakahashi97@gmail.com>
2024-03-12 18:48:17 -0700

    Added UI testing for EmailVerify page dialog/snackbar

--- app/src/androidTest/java/com/chartracker/screens/EmailVerifyScreenTest.kt
+++ app/src/androidTest/java/com/chartracker/screens/EmailVerifyScreenTest.kt
@@ -1,12 +1,16 @@
 package com.chartracker.screens
 
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.test.assertIsDisplayed
+import androidx.compose.ui.test.hasAnyAncestor
 import androidx.compose.ui.test.hasClickAction
 import androidx.compose.ui.test.hasText
+import androidx.compose.ui.test.isDialog
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithContentDescription
 import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.performClick
 import com.chartracker.ui.auth.EmailVerifyScreen
 import org.junit.Before
 import org.junit.Rule
@@ -16,20 +20,22 @@ import org.junit.Test
 class EmailVerifyScreenTest {
     @get: Rule
     val composeTestRule = createComposeRule()
+    private val failedReload = mutableStateOf(false)
+    private val emailSent = mutableStateOf(false)
 
     @Before
     fun setupEmailVerifyScreen(){
         composeTestRule.setContent {
             EmailVerifyScreen(
                 email = "test@email.com",
-                sendEmail = { /*TODO*/ },
-                changeEmail = { /*TODO*/ },
+                sendEmail = { /**/ },
+                changeEmail = { /**/ },
                 checkVerification = { -> false },
-                failedReload = false,
-                resetFailedReload = {},
-                emailSent = false,
-                resetEmailSent = {},
-                navToStories = { /*TODO*/ }) {
+                failedReload = failedReload.value,
+                resetFailedReload = {failedReload.value = false},
+                emailSent = emailSent.value,
+                resetEmailSent = {emailSent.value = false},
+                navToStories = { /**/ }) {
 
             }
         }
@@ -72,6 +78,49 @@ class EmailVerifyScreenTest {
                         and
                         hasText("I'm verified!")
             )
+            .assertIsDisplayed()
+    }
+
+    @Test
+    fun failedReloadTest(){
+        // activate the dialog
+        failedReload.value = true
+
+        composeTestRule
+            .onNode(
+                hasAnyAncestor(isDialog())
+                        and
+                        hasText("Error with account!")
+            )
+            .assertIsDisplayed()
+
+        composeTestRule
+            .onNode(
+                hasAnyAncestor(isDialog())
+                        and
+                        hasClickAction()
+                        and
+                        hasText("Dismiss")
+            )
+            .assertIsDisplayed()
+            .performClick()
+
+        composeTestRule
+            .onNode(
+                hasAnyAncestor(isDialog())
+                        and
+                        hasText("Error with account!")
+            )
+            .assertDoesNotExist()
+    }
+
+    @Test
+    fun emailSentTest(){
+        // activate snackbar
+        emailSent.value = true
+
+        composeTestRule
+            .onNodeWithText("Email sent!")
             .assertIsDisplayed()
     }
 }
