1954e050d1c03bc7ada2d23069292107fb58e274
rhtakaha <rytakahashi97@gmail.com>
2024-04-12 13:42:11 -0700

    Test EmailVerifyScreen

---
+++ app/src/test/java/com/chartracker/viewModelTests/EmailVerifyViewModelTest.kt
@@ -0,0 +1,48 @@
+package com.chartracker.viewModelTests
+
+import com.chartracker.MainDispatcherRule
+import com.chartracker.database.MockUserDB
+import com.chartracker.viewmodels.auth.EmailVerifyViewModel
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.advanceUntilIdle
+import kotlinx.coroutines.test.runTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.robolectric.RobolectricTestRunner
+
+@RunWith(RobolectricTestRunner::class)
+class EmailVerifyViewModelTest {
+    private val mockUserDB = MockUserDB()
+    private lateinit var viewmodel: EmailVerifyViewModel
+
+    @ExperimentalCoroutinesApi
+    @get:Rule
+    val mainDispatcherRule = MainDispatcherRule()
+
+    @Before
+    fun setup() = runTest {
+        viewmodel = EmailVerifyViewModel(mockUserDB)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun initTest() = runTest {
+        //allows for the internal coroutines to run
+        advanceUntilIdle()
+
+        assert(viewmodel.emailSent.value)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun verificationTest() = runTest {
+        val output = viewmodel.isEmailVerified()
+        //allows for the internal coroutines to run
+        advanceUntilIdle()
+
+        assert(output)
+        assert(!viewmodel.failedReload.value)
+    }
+}
