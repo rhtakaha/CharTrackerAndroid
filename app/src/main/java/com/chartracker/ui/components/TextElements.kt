97d37780f36537c5510f2fb616ddb391eabc8f34
rhtakaha <rytakahashi97@gmail.com>
2024-05-31 22:03:32 -0700

    started making the components for the faction screen.

--- app/src/main/java/com/chartracker/ui/components/TextElements.kt
+++ app/src/main/java/com/chartracker/ui/components/TextElements.kt
@@ -5,15 +5,23 @@ import android.content.res.Configuration.UI_MODE_NIGHT_YES
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Cancel
+import androidx.compose.material.icons.filled.Done
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material.icons.filled.Visibility
 import androidx.compose.material.icons.filled.VisibilityOff
+import androidx.compose.material3.Button
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.Composable
@@ -22,8 +30,10 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.ImeAction
@@ -180,5 +190,132 @@ fun PreviewTextEntryHolderEmail(){
             onTyping = { newInput -> text = newInput },
             isEmail = true
         )
+    }
+}
+
+/* composable for entering text and adding it*/
+@Composable
+fun TextEntryAndAddHolder(
+    @StringRes label: Int,
+    text: String,
+    onTyping: (String) -> Unit,
+    onAdd: (String) -> Unit
+) {
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        TextField(
+            value = text,
+            onValueChange = onTyping,
+            label = { Text(stringResource(id = label)) },
+            singleLine = true,
+            textStyle = TextStyle(),
+            shape = MaterialTheme.shapes.small,
+            keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next),
+            modifier = Modifier
+                .padding(8.dp)
+        )
+        Button(onClick = { onAdd(text) }) {
+            Text(text = stringResource(id = R.string.add))
+        }
+    }
+}
+
+@Composable
+fun FactionItem(
+    @StringRes label: Int,
+    text: String,
+    color: Long,
+    onTyping: (String) -> Unit,
+    onUpdate: (String, Int) -> Unit
+) {
+    var editing by rememberSaveable { mutableStateOf(false) }
+    Row(
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        IconButton(onClick = {editing = true}){
+            Icon(imageVector  = Icons.Filled.Edit, stringResource(id = R.string.edit))
+        }
+        if (editing){
+            TextField(
+                value = text,
+                onValueChange = onTyping,
+                label = { Text(stringResource(id = label)) },
+                singleLine = true,
+                textStyle = TextStyle(),
+                shape = MaterialTheme.shapes.small,
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Next)
+            )
+        }else{
+            Text(text = text)
+        }
+        Spacer(modifier = Modifier.width(8.dp))
+
+        //Color block
+        Surface(color = Color(color), modifier = Modifier.size(25.dp)) {
+
+        }
+        //TODO: add the color selector here by clicking on the color square
+
+        if (editing){
+            IconButton(
+                onClick = { /*TODOonUpdate(text, color)*/ },
+                modifier = Modifier.size(20.dp)
+            ){
+                Icon(imageVector  = Icons.Filled.Done, stringResource(id = R.string.submit))
+            }
+            IconButton(
+                onClick = {editing = false},
+                modifier = Modifier.size(20.dp)
+            ){
+                Icon(imageVector  = Icons.Filled.Cancel, stringResource(id = R.string.cancel))
+            }
+        }else{
+            Spacer(modifier = Modifier.size(20.dp))
+            Spacer(modifier = Modifier.size(20.dp))
+        }
+    }
+}
+
+@Preview(
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Regular Dark Mode")
+@Preview(
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "Regular Light Mode")
+@Composable
+fun PreviewTextEntryAndAddHolder(){
+    var text by remember { mutableStateOf("") }
+    CharTrackerTheme {
+        TextEntryAndAddHolder(
+            label = R.string.faction_hint,
+            text = text,
+            onTyping = { newInput -> text = newInput },
+            onAdd = {}
+        )
+    }
+}
+
+@Preview(
+    uiMode = UI_MODE_NIGHT_YES,
+    name = "Regular Dark Mode")
+@Preview(
+    uiMode = UI_MODE_NIGHT_NO,
+    name = "Regular Light Mode")
+@Composable
+fun PreviewFactionItem(){
+    var text by remember { mutableStateOf("Straw Hat Pirates") }
+    CharTrackerTheme {
+        Surface {
+            FactionItem(
+                label = R.string.faction_hint,
+                text = text,
+                color = 0xFF0000FF,
+                onTyping = { newInput -> text = newInput }
+            ) { _, _ -> }
+        }
+
     }
 }
